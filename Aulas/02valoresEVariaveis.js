591f93b92c24f2845ec2b276f0d71ee3446ee7fd
André Luis Suckow <alskw0911@gmail.com>
2023-05-31 14:30:13 -0300

    01-04 aulas

---
+++ Aulas/02valoresEVariaveis.js
@@ -0,0 +1,7 @@
+var idade = 32;
+console.log(idade);
+
+var nome = "André";
+
+console.log(nome);
+var curso = "code Academy - Ifood";
---
+++ Aulas/03ExemploVariaveis.js
@@ -0,0 +1,7 @@
+var nome = "André"; // variável global
+let idade = 32; // variável não global
+const curso = "Cubos Academy" // variável que não altera
+console.log(nome, idade, curso);
+idade = 26;
+nome = "Martha";
+console.log(nome, idade, curso);
